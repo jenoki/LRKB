2b1988b887b498969bdb0975572bfb114a9454ca
Junya Enoki <jenoki@ricky.local>
2018-07-15 10:12:43 +0900

    Initial commit

---
+++ hid_msd_basic/firmware/hid_msd_basic.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,20 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# pic32mx_usb_sk2_int_dyn configuration
+CND_ARTIFACT_DIR_pic32mx_usb_sk2_int_dyn=dist/pic32mx_usb_sk2_int_dyn/production
+CND_ARTIFACT_NAME_pic32mx_usb_sk2_int_dyn=hid_msd_basic.X.production.hex
+CND_ARTIFACT_PATH_pic32mx_usb_sk2_int_dyn=dist/pic32mx_usb_sk2_int_dyn/production/hid_msd_basic.X.production.hex
+CND_PACKAGE_DIR_pic32mx_usb_sk2_int_dyn=${CND_DISTDIR}/pic32mx_usb_sk2_int_dyn/package
+CND_PACKAGE_NAME_pic32mx_usb_sk2_int_dyn=hidmsdbasic.x.tar
+CND_PACKAGE_PATH_pic32mx_usb_sk2_int_dyn=${CND_DISTDIR}/pic32mx_usb_sk2_int_dyn/package/hidmsdbasic.x.tar
+# pic32mz_ef_sk_int_dyn configuration
+CND_ARTIFACT_DIR_pic32mz_ef_sk_int_dyn=dist/pic32mz_ef_sk_int_dyn/production
+CND_ARTIFACT_NAME_pic32mz_ef_sk_int_dyn=hid_msd_basic.X.production.hex
+CND_ARTIFACT_PATH_pic32mz_ef_sk_int_dyn=dist/pic32mz_ef_sk_int_dyn/production/hid_msd_basic.X.production.hex
+CND_PACKAGE_DIR_pic32mz_ef_sk_int_dyn=${CND_DISTDIR}/pic32mz_ef_sk_int_dyn/package
+CND_PACKAGE_NAME_pic32mz_ef_sk_int_dyn=hidmsdbasic.x.tar
+CND_PACKAGE_PATH_pic32mz_ef_sk_int_dyn=${CND_DISTDIR}/pic32mz_ef_sk_int_dyn/package/hidmsdbasic.x.tar
